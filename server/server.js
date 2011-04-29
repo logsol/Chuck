d1cdd6d635a60dd8c0bf16385acc708738f087e0
logsol <fartman@gmx.de>
2011-04-29 16:07:32 +0100

    Added sendToAll and server resets when host leaves

--- server/server.js
+++ server/server.js
@@ -8,38 +8,79 @@ function Client(socket, id){
 	this.name = null;
 	this.is_host = (id == 0);
 	this.id = id;
-    this.socket.write("Welcome " + (this.is_host ? 'host' : 'client') + "\n");
-    
-    var self = this;
-    socket.on("data", function(data) {
-        var type = parseInt(data.toString("utf8", 0, 1));
-        var message = "";
-        if(data.length > 1) {
-            message = data.toString("utf8", 1);
-        }
+	this.socket.write("Welcome " + (this.is_host ? 'host' : 'client') + "\n");
 
-        switch (type) {
-        case 0: // echo
-            socket.write(message);
-            break;
-        case 1: // my id
-            socket.write(self.id + "\n");
-            break;
-        case 2: // set my name
-            self.name = message.trim();
-            socket.write(self.name + "\n");
-            break;
-        case 3: // am I host?
-            socket.write(self.is_host + "\n");
-            break;
-        case 4: // send to client id
-            break;
-        case 5: // send to all
-            break;
-        case 6: // send to array
-            break;
-        }
-    });
+	var self = this;
+
+	this.socket.on("data", function(data) {
+		var type = parseInt(data.toString('utf8', 0, 1));
+		var message = (data.length > 1) ? data.toString('utf8', 1).trim() : '';
+
+		switch (type) {
+			case 0: // echo
+				socket.write(message);
+				break;
+			case 1: // my id
+				socket.write(self.id + "\n");
+				break;
+			case 2: // set my name
+				self.name = message;
+				socket.write(self.name + "\n");
+				break;
+			case 3: // am I host?
+				socket.write(self.is_host + "\n");
+				break;
+			case 4: // send to host
+				//getClientById(1).send(message);
+				host.send(message);
+				break;
+			case 5: // send to all
+				sendToAll(message);
+				break;
+			case 6: // send to client
+				break;
+			case 7: // send to group
+				break;
+		}
+	});
+
+	this.socket.on("end", function(){
+		clients.splice(clients.indexOf(self), 1);
+		if(self === host)
+		{
+			for(var x in clients){
+				clients[x].socket.end('host has left, sorry, goodbye' + "\n");
+				clients[x] = null;
+			}
+			clients = [];
+			host = null;
+			new_client_id = 0;
+		} 
+		else {
+			sendToAll('client ' + self.id + ' has left.' + "\n");
+		}
+		self = null;
+	});
+
+	this.send = function(message){
+		this.socket.write(message + "\n");
+	}
+}
+
+function sendToAll(message){
+	for(var x in clients)
+	{
+		clients[x].send(message);
+	}
+}
+
+function getClientById(id){
+	for(var x in clients)
+	{
+		if(clients[x].id == id){
+			return clients[x];
+		}
+	}
 }
 
 var server = net.createServer(function(socket) {
@@ -55,7 +96,6 @@ var server = net.createServer(function(socket) {
 server.listen(6937, "127.0.0.1");
 
 
-// call: nc localhost 6937
 
 // helper
 String.prototype.trim = function() {
@@ -67,3 +107,5 @@ String.prototype.ltrim = function() {
 String.prototype.rtrim = function() {
 	return this.replace(/\s+$/,"");
 }
+
+// call: nc localhost 6937
