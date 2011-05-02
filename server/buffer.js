ac4c578a07405c32d89017c52ebb47462fb8f779
logsol <fartman@gmx.de>
2011-05-02 12:24:46 +0100

    Stream buffer function

---
+++ server/buffer.js
@@ -0,0 +1,64 @@
+// Buffering Tests
+
+var input = [
+	"0087Dies ist ein Songt", 
+	"ext. Hallo mein Sch", 
+	"atz, ich liebe D", 
+	"ich, du bist di", 
+	"e einzige f", "ür mich!",
+	"0034Dies ist der z",
+	"weite Teil des Tests"
+];
+
+var buffer = "";
+var streamLength = 0;
+var counter = 0;
+
+var lb = 4;
+
+function onData()
+{
+	data = input[counter];
+
+	console.log("- onData: " + data);
+
+	if(buffer == "")
+        {
+		streamLength = parseInt(data.substr(0, lb), 10);
+		data = data.substr(lb);
+		
+		if(streamLength == data.length)
+		{
+			processData(data);
+			streamLength = 0;
+		}
+		else if(streamLength > data.length)
+		{
+			buffer = data;
+		}
+        }
+	else 
+	{
+		if((buffer.length + data.length) == streamLength)
+		{
+			processData(buffer + data);
+			streamLength = 0;
+			buffer = "";
+		}
+		else
+		{
+			buffer += data;
+		}
+	}
+
+	counter++;
+	if(input[counter]) setTimeout(onData, 300);
+}
+
+function processData(str)
+{
+	console.log("\n  => processing: " + str + "\n");
+}
+
+
+onData();
